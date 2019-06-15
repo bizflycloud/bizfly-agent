0a75fd30f4afa6e5ceeb6413881d473aefcb1fee
LE Manh Cuong <cuong.manhle.vn@gmail.com>
2019-06-15 22:32:52 +0700

    refactoring

--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
-	"fmt"
+
 	"github.com/prometheus/client_golang/prometheus/push"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/node_exporter/collector"
 	"gopkg.in/alecthomas/kingpin.v2"
-	"io/ioutil"
-	"strings"
 )
-
-
-
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
-}
 
 func main() {
 	var pushGatewayAddress = kingpin.Flag("pushgateway.address", "The address of pushgateway server").Default("http://localhost/metrics").String()
@@ -35,23 +27,26 @@ func main() {
 
 	nc, err := collector.NewNodeCollector(defaultCollectors...)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to create new collector: %s\n", err.Error())
 	}
 	data, err := ioutil.ReadFile("/var/lib/cloud/data/instance-id")
-	check(err)
-	instance_id := strings.TrimSuffix(string(data), "\n")
+	if err != nil {
+		log.Fatalf("failed to read instance id file: %s\\n", err.Error())
+	}
+	instanceID := strings.TrimSuffix(string(data), "\n")
 
-	pusher := push.New(*pushGatewayAddress, "bizfly-agent").Client(httpClient)
-	pusher.Grouping("instance_id", string(instance_id))
-	pusher.Collector(nc)
+	pusher := push.New(*pushGatewayAddress, "bizfly-agent").
+		Client(httpClient).
+		Grouping("instance_id", instanceID).
+		Collector(nc)
 
 	if err := pusher.Push(); err != nil {
-		log.Errorf("failed to make initial push to push gateway: %v", err)
+		log.Errorf("failed to make initial push to push gateway: %s\n", err.Error())
 	}
 	for {
 		time.Sleep(time.Second * time.Duration(*waitDuration))
 		if err := pusher.Push(); err != nil {
-			log.Errorf("failed to push to push gateway: %v", err)
+			log.Errorf("failed to push to push gateway: %s\n", err.Error())
 		}
 	}
 }
